fcae0cce766f1ccd8146cbd26550724aa79bb503
Anthony <anthony.caccavella@gmail.com>
2017-11-12 16:53:41 -0700

    fixing

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -13,7 +13,7 @@ class Login extends Component {
     render() {
         return (
             <div className="loginwrapper">
-                <div className="starsimage"><img src="https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/moving-through-stars-in-space_-1zccenlb__F0000.png"/></div>
+                <div className="starsimage"><img alt="background stars" src="https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/moving-through-stars-in-space_-1zccenlb__F0000.png"/></div>
                 <div className="sitetitle">Constellation Competition
                 <div className="sitesub">Reach for the stars</div>
                 </div>
